71817d7c2e0d55338444f7caaf47aac0a103f733
Chris Taylor <chris11.taylor@nttdata.com>
2024-04-19 10:11:19 -0500

    Fixes for lint

--- examples/minimal/versions.tf
+++ examples/minimal/versions.tf
@@ -14,6 +14,10 @@ terraform {
   required_version = ">= 1.5.0, <= 1.5.5"
 
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.14"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.6"
